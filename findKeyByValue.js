173e4c50998e129f33dc31dfd3f3c66887df6e7e
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-05 18:45:29 -0500

    Implement findKeyByValue to search for a key on an object where its value matches a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  const objectArray = Object.keys(object);
+  for (const obj of objectArray) {
+    if (object[obj] === value) {
+      return obj;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
