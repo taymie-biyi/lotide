ef539de2a61a768cc06e3ae8338e691dc5939b39
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-07 13:41:45 -0500

    Scan the object and return the first key for which the callback returns a truthy value

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  result ='';
+  const objectArray = Object.keys(object);
+  for (const obj of objectArray) {
+    if (callback(object[obj])) {
+      result = obj;
+      break;
+    }
+  }
+  return result;
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
