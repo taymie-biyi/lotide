4be07a97d498fe7aa4bc0768265ff5cf59aecdb8
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-11 14:51:15 -0500

    Refractor countLetters file and add it's test file

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,9 +1,13 @@
+const assert = require('chai').assert;
+
 const countLetters = require('../countLetters');
 
-const assertEqual = require('../assertEqual');
 
+describe("#without", () => {
+  it("returns { i: 5, l: 1, v: 1, e: 2, n: 3, w: 1, p: 1, g: 1 }, when the source code is 'i live in winnipeg'", () => {
 
-const test = "i live in winnipeg";
-console.log(countLetters(test));
+    const sourceCode = 'i live in winnipeg';
 
-assertEqual(test, "i live in winnipeg");
+    assert.deepEqual(countLetters(sourceCode), {i: 5, l: 1, v: 1, e: 2, n: 3, w: 1, p: 1, g: 1});
+  });
+});
