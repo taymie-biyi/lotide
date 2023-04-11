2d667e4369613e7e1075886998834bbcc6aabca7
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-10 23:18:04 -0500

    Refractor findKey file and add it's test file

--- findKey.js
+++ findKey.js
@@ -1,28 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const findKey = function(object, callback) {
-  result ='';
+  let result = '';
   const objectArray = Object.keys(object);
-  for (const obj of objectArray) {
-    if (callback(object[obj])) {
-      result = obj;
+  for (const val of objectArray) {
+    if (callback(object[val])) {
+      result = val;
       break;
     }
   }
   return result;
 };
 
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2))
+
+module.exports = findKey;
