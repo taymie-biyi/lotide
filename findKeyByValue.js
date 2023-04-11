6e6ac12d60658be92b9c75f078ce1e2cd5e828b4
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-10 22:59:08 -0500

    Refractor findKeyByValue file and add it's test file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const findKeyByValue = function(object, value) {
   const objectArray = Object.keys(object);
@@ -16,11 +9,5 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
