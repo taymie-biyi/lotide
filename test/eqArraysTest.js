5f834339477050d5ba6a3f431e40f293a1c5e099
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-11 14:03:42 -0500

    Refractor eqArrays file and add it's test file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,13 +1,35 @@
+const assert = require('chai').assert;
+
 const eqArrays = require('../eqArrays');
 
-const assertEqual = require('../assertEqual');
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", 2, "3"]), false); // => false
-
+describe("#eqObjects", () => {
+  
+  it("returns false when the length of array1 and array2 are not the same", () => {
+    const array1 = [1, 2, 3];
+    const array2 = [1, 2, 3, 4];
+    
+    assert.isFalse(eqArrays(array1, array2));
+  });
+  
+  it("returns false when the data type of a value in array1 and array2 are not the same", () => {
+    const array1 = ["1", "2", "3"];
+    const array2 = ["1", 2, "3"];
+    
+    assert.isFalse(eqArrays(array1, array2));
+  });
+  
+  it("returns false when values of  array1 and array2 are not the same", () => {
+    const array1 = [1, 2, 3];
+    const array2 = [3, 2, 1];
+    
+    assert.isFalse(eqArrays(array1, array2));
+  });
+  
+  it("returns true when values, length and data type of array1 and array2 are the same", () => {
+    const array1 = ["1", "2", "3"];
+    const array2 = ["1", "2", "3"];
+  
+    assert.isTrue(eqArrays(array1, array2));
+  });
+});
