7bb9fa844cab2faa18e32eb0ce5836ad0e4ca339
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-11 00:10:27 -0500

    eslist headTest file

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,6 @@
 
 const assert = require('chai').assert;
-const head   = require('../head');
+const head = require('../head');
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
@@ -8,15 +8,14 @@ describe("#head", () => {
   });
 
   it("returns 5 for [5,6,7]", () => {
-    assert.strictEqual(head([5,6,7]), 5); 
+    assert.strictEqual(head([5,6,7]), 5);
   });
 
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5');
   });
 
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
   });
-
 });
