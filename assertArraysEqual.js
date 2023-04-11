a2e5b418dede07ed06fa700a903518b35ae7c669
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-11 12:15:22 -0500

    Refractor assertArraysEqual file and add it's test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,18 @@
-const eqArrays = function(arr1,arr2) {
-  let response = '';
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      response = true;
-    } else {
-      return false;
-    }
-  }
-  return response;
-};
+
+const eqArrays = require('./eqArrays');
+
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
+  if (!(eqArrays(arr1, arr2))) {
     console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    return;
   }
+
+  console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+// const assert = require('chai').assert;
+
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
