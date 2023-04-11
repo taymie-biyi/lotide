02b3e438d92c4ad2a13e6275dbb8a46c2e9d24f9
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-10 21:26:01 -0500

    Refractor assertEqual file and add it's test file

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,11 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
+  if (actual !== expected) {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return;
   }
+
+  console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
