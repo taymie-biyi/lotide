2f15d746d4dc4aa79d4d6a0af99c69222d7b75b0
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-11 14:34:21 -0500

    Refractor countOnly file and add it's test file

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,23 +1,87 @@
+const assert = require('chai').assert;
+
 const countOnly = require('../countOnly');
 
-const assertEqual = require('../assertEqual');
 
+describe("#countOnly", () => {
+  
+  it("returns undefined because 'Karima' is not listed in the array of names and it's value is true in the comparison object" , () => {
+    const names = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    
+    const compareName = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
+    const result = countOnly(names, compareName);
+    
+    assert.strictEqual(result["Karima"], undefined);
+  });
+  
+  it("returns undefined because 'Agouhanna' appears in the list of names, but is listed as false in the comparison array of names" , () => {
+    
+    const names = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    
+    const compareName = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
+    const result = countOnly(names, compareName);
+    
+    assert.strictEqual(result["Karima"], undefined);
+  });
+  
+  it("returns '1' because 'Jason' appears once in the array of names and it's value is true in the comparison object", () => {
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
+    const names = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    
+    const compareName = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
+    const result = countOnly(names, compareName);
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+    assert.strictEqual(result["Jason"], 1);
+  });
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+  it("returns '2' because 'Fang' is appears twice in the array of names and it's value is true in the comparison object" , () => {
+
+    const names = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    
+    const compareName = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
+    const result = countOnly(names, compareName);
+
+    assert.strictEqual(result["Fang"], 2);
+  });
+
+});
